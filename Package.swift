73f3e3a1b4dea9eafe19da6906899123422f925b
4bitCrew <matt@4bitcrew.com>
2020-06-13 14:52:29 -0700

    Changed: the min OS

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Scripts",
-    platforms: [ .macOS(.v10_12)
+    platforms: [ .macOS(.v10_15)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
